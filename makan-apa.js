f888c02ac7426d9da1385c70b4609519664ffd42
dhannywijaya <dhannywijaya1997@gmail.com>
2023-06-08 18:16:25 +0700

    revisi ke sekian kali

--- makan-apa.js
+++ makan-apa.js
@@ -1,61 +1,64 @@
 const database = {
-    RiceandNoodle: {
-        RiceandNoodle1: { name: "Nasi Goreng", id: "RiceandNoodle1", class: "RiceandNoodle-nasi card-styling", img: "" },
-        RiceandNoodle2: { name: "Nasi Padang", id: "RiceandNoodle2", class: "RiceandNoodle-nasi card-styling", img: "" },
-        RiceandNoodle3: { name: "Nasi Uduk",   id: "RiceandNoodle3", class: "RiceandNoodle-nasi card-styling", img: "" },
-        RiceandNoodle4: { name: "Nasi Kuning", id: "RiceandNoodle4", class: "RiceandNoodle-nasi card-styling", img: "" },
-        RiceandNoodle5: { name: "Nasi Liwet",  id: "RiceandNoodle5", class: "RiceandNoodle-nasi card-styling", img: "" },
-        RiceandNoodle6: { name: "Mie Goreng",  id: "RiceandNoodle6", class: "RiceandNoodle-mie card-styling",  img: "" },
-        RiceandNoodle7: { name: "Mie Ayam",    id: "RiceandNoodle7", class: "RiceandNoodle-mie card-styling",  img: "" },
-        RiceandNoodle8: { name: "Mie Aceh",    id: "RiceandNoodle8", class: "RiceandNoodle-mie card-styling",  img: "" },
-        RiceandNoodle9: { name: "Mie Jawa",    id: "RiceandNoodle9", class: "RiceandNoodle-mie card-styling",  img: "" },
-        RiceandNoodle10: { name: "Mie Bakso",  id: "RiceandNoodle10", class: "RiceandNoodle-nasimie card-styling", img: "" }
+    "Rice&Noodle" : {
+        "Rice&Noodle1": { name: "Nasi Goreng", id: "Rice&Noodle1", class: "Rice&Noodle-Nasi card-styling",  img: "https://i.ibb.co/9nPTNM5/Nasi-Goreng.png" },
+        "Rice&Noodle2": { name: "Nasi Padang", id: "Rice&Noodle2", class: "Rice&Noodle-Nasi card-styling",  img: "https://i.ibb.co/fS5yMwv/Nasi-Padang.png" },
+        "Rice&Noodle3": { name: "Nasi Uduk",   id: "Rice&Noodle3", class: "Rice&Noodle-Nasi card-styling",  img: "https://i.ibb.co/SRJWH8f/Nasi-Uduk.png" },
+        "Rice&Noodle4": { name: "Nasi Kuning", id: "Rice&Noodle4", class: "Rice&Noodle-Nasi card-styling",  img: "https://i.ibb.co/WpZ1ypR/Nasi-Kuning.png" },
+        "Rice&Noodle5": { name: "Nasi Liwet",  id: "Rice&Noodle5", class: "Rice&Noodle-Nasi card-styling",  img: "https://i.ibb.co/ZYzNyjS/Nasi-Liwet.png" },
+        "Rice&Noodle6": { name: "Mie Goreng",  id: "Rice&Noodle6", class: "Rice&Noodle-Mie card-styling",   img: "https://i.ibb.co/kmrzbrC/Mie-Goreng.png" },
+        "Rice&Noodle7": { name: "Mie Ayam",    id: "Rice&Noodle7", class: "Rice&Noodle-Mie card-styling",   img: "https://i.ibb.co/sH3WCJ5/Mie-Ayam.png" },
+        "Rice&Noodle8": { name: "Mie Aceh",    id: "Rice&Noodle8", class: "Rice&Noodle-Kuah card-styling",  img: "https://i.ibb.co/HnX0jBY/Mie-Aceh.png" },
+        "Rice&Noodle9": { name: "Mie Jawa",    id: "Rice&Noodle9", class: "Rice&Noodle-Mie card-styling",   img: "https://i.ibb.co/fN8rSjr/Mie-Jawa.png" },
+        "Rice&Noodle10": { name: "Mie Bakso",  id: "Rice&Noodle10", class: "Rice&Noodle-Kuah card-styling", img: "https://i.ibb.co/bNftSJF/Mie-Bakso.png" }
     },
     Salads: {
-        Salads1: { name: "Gado-Gado",   id: "Salads1", class: "Salads-Indonesian card-styling", img: "" },
-        Salads2: { name: "Karedok",     id: "Salads2", class: "Salads-Indonesian card-styling", img: "" },
-        Salads3: { name: "Pecel",       id: "Salads3", class: "Salads-Indonesian card-styling", img: "" },
-        Salads4: { name: "Gudeg Jogja", id: "Salads4", class: "Salads-Indonesian card-styling", img: "" },
-        Salads5: { name: "Gudeg Solo",  id: "Salads5", class: "Salads-Indonesian card-styling", img: "" },
-        Salads6: { name: "Caesar Salad",  id: "Salads6", class: "Salads-American card-styling", img: "" },
-        Salads7: { name: "Olive Oil Salad",  id: "Salads7", class: "Salads-American card-styling", img: "" },
-        Salads8: { name: "Pasta Salad",  id: "Salads8", class: "Salads-American card-styling", img: "" }
+        Salads1: { name: "Gado-Gado",    id: "Salads1", class: "Salads-Indonesian card-styling",  img: "https://i.ibb.co/q5910kb/Gado-gado.png" },
+        Salads2: { name: "Karedok",      id: "Salads2", class: "Salads-Indonesian card-styling",  img: "https://i.ibb.co/pJph6nR/Karedok.png" },
+        Salads3: { name: "Pecel",        id: "Salads3", class: "Salads-Indonesian card-styling",  img: "https://i.ibb.co/wrMYzWx/Pecel.png" },
+        Salads4: { name: "Gudeg Jogja",  id: "Salads4", class: "Salads-Traditional card-styling", img: "https://i.ibb.co/sFkx8Bm/Gudeg-Jogja.png" },
+        Salads5: { name: "Gudeg Solo",   id: "Salads5", class: "Salads-Traditional card-styling", img: "https://i.ibb.co/FBQ8ZJJ/Gudeg-Solo.png" },
+        Salads6: { name: "Caesar Salad", id: "Salads6", class: "Salads-American card-styling",    img: "https://i.ibb.co/S6gdwkD/image.png" },
+        Salads7: { name: "Green Salad",  id: "Salads7", class: "Salads-American card-styling",    img: "https://i.ibb.co/NLMgv6m/image.png" },
+        Salads8: { name: "Pasta Salad",  id: "Salads8", class: "Salads-American card-styling",    img: "https://i.ibb.co/bK5732v/image.png" }
     },
     Soups: {
-        Soups1: { name: "Soto Ayam",       id: "Soups1", class: "Soups-Soto card-styling", img: "" },
-        Soups2: { name: "Soto Betawi",     id: "Soups2", class: "Soups-Soto card-styling", img: "" },
-        Soups3: { name: "Soto Medan",      id: "Soups3", class: "Soups-Soto card-styling", img: "" },
-        Soups4: { name: "Soto Ambengan",   id: "Soups4", class: "Soups-Soto card-styling", img: "" },
-        Soups5: { name: "Soto Banjar",     id: "Soups5", class: "Soups-Soto card-styling", img: "" },
-        Soups6: { name: "Sop Ayam",        id: "Soups6", class: "Soups-Eastern card-styling", img: "" },
-        Soups7: { name: "Tom Yum",         id: "Soups7", class: "Soups-Eastern card-styling", img: "" },
-        Soups8: { name: "Mushroom Soup",   id: "Soups8", class: "Soups-Eastern card-styling", img: "" }
+        Soups1: { name: "Soto Ayam",     id: "Soups1", class: "Soups-Fresh card-styling",   img: "https://i.ibb.co/2h7SGSw/Soto-Ayam.png" },
+        Soups2: { name: "Soto Betawi",   id: "Soups2", class: "Soups-Fresh card-styling",   img: "https://i.ibb.co/PC0F9Bs/Soto-Betawi.png" },
+        Soups3: { name: "Soto Medan",    id: "Soups3", class: "Soups-Soto card-styling",    img: "https://i.ibb.co/1ryMP5h/Soto-Medan.png" },
+        Soups4: { name: "Soto Ambengan", id: "Soups4", class: "Soups-Soto card-styling",    img: "https://i.ibb.co/s2hs7RF/Soto-Ambengan.png" },
+        Soups5: { name: "Soto Banjar",   id: "Soups5", class: "Soups-Soto card-styling",    img: "https://i.ibb.co/hZ2jZRK/Soto-Banjar.png" },
+        Soups6: { name: "Sop Ayam",      id: "Soups6", class: "Soups-Eastern card-styling", img: "https://i.ibb.co/BNzqJq0/Sop-Ayam.png" },
+        Soups7: { name: "Tom Yum",       id: "Soups7", class: "Soups-Eastern card-styling", img: "https://i.ibb.co/gJGQLHj/Tomyum.png" },
+        Soups8: { name: "Mushroom Soup", id: "Soups8", class: "Soups-Eastern card-styling", img: "https://i.ibb.co/TLC6npH/Mushroom-Soup.png" }
     },
     Grilled: {
-        Grilled1: { name: "Satay Ayam",   id: "Grilled1", class: "Grilled-Satay card-styling", img: "" },
-        Grilled2: { name: "Satay Kambing",id: "Grilled2", class: "Grilled-Satay card-styling", img: "" },
-        Grilled3: { name: "Ayam Bakar",   id: "Grilled3", class: "Grilled-Main card-styling",  img: "" },
-        Grilled4: { name: "Ikan Bakar",   id: "Grilled4", class: "Grilled-Main card-styling",  img: "" },
-        Grilled5: { name: "Steak",        id: "Grilled5", class: "Grilled-Main card-styling",  img: "" },
-        Grilled6: { name: "Satay Lilit",  id: "Grilled6", class: "Grilled-Satay card-styling", img: "" },
-        Grilled7: { name: "Satay Padang", id: "Grilled7", class: "Grilled-Satay card-styling", img: "" },
-        Grilled8: { name: "Satay Taichan",id: "Grilled8", class: "Grilled-Satay card-styling", img: "" },
-        Grilled9: { name: "Satay Kerang", id: "Grilled9", class: "Grilled-Satay card-styling", img: "" }
+        Grilled1: { name: "Satay Ayam",    id: "Grilled1", class: "Grilled-Satay card-styling", img: "https://i.ibb.co/YPCcH7W/Sate-Ayam.png" },
+        Grilled2: { name: "Satay Kambing", id: "Grilled2", class: "Grilled-Satay card-styling", img: "https://i.ibb.co/19N9dXJ/Sate-Kambing.png" },
+        Grilled3: { name: "Ayam Bakar",    id: "Grilled3", class: "Grilled-Main card-styling",  img: "https://i.ibb.co/gdNzXNd/Ayam-Bakar.png" },
+        Grilled4: { name: "Ikan Bakar",    id: "Grilled4", class: "Grilled-Main card-styling",  img: "https://i.ibb.co/ChXkTc4/Ikan-Bakar.png" },
+        Grilled5: { name: "Steak",         id: "Grilled5", class: "Grilled-Main card-styling",  img: "https://i.ibb.co/Z8BnYwM/Steak.png" },
+        Grilled6: { name: "Satay Lilit",   id: "Grilled6", class: "Grilled-Hits card-styling",  img: "https://i.ibb.co/TmXQn6Y/Sate-Lilit.png" },
+        Grilled7: { name: "Satay Padang",  id: "Grilled7", class: "Grilled-Satay card-styling", img: "https://i.ibb.co/3k9zjs2/Sate-Padang.png" },
+        Grilled8: { name: "Satay Taichan", id: "Grilled8", class: "Grilled-Hits card-styling",  img: "https://i.ibb.co/KzdNFGK/Sate-Taichan.png" },
+        Grilled9: { name: "Satay Kerang",  id: "Grilled9", class: "Grilled-Hits card-styling",  img: "https://i.ibb.co/3R9M8KS/Sate-Kerang.png" }
     },
     Desserts: {
-        Desserts1: { name: "Es Teler",     id: "Desserts1", class: "Desserts-Es card-styling",      img: "" },
-        Desserts2: { name: "Cendol",       id: "Desserts2", class: "Desserts-Es card-styling",      img: "" },
-        Desserts3: { name: "Bubur Sumsum", id: "Desserts3", class: "Desserts-Jajanan card-styling", img: "" },
-        Desserts4: { name: "Kue Lapis",    id: "Desserts4", class: "Desserts-Jajanan card-styling", img: "" },
-        Desserts5: { name: "Klepon",       id: "Desserts5", class: "Desserts-Jajanan card-styling", img: "" }
+        Desserts1: { name: "Es Teler",       id: "Desserts1", class: "Desserts-Es card-styling",      img: "https://i.ibb.co/6s7MktR/es-teler.png" },
+        Desserts2: { name: "Cendol",         id: "Desserts2", class: "Desserts-Es card-styling",      img: "https://i.ibb.co/kBBSYmG/cendol.png" },
+        Desserts3: { name: "Bubur Sumsum",   id: "Desserts3", class: "Desserts-Jajanan card-styling", img: "https://i.ibb.co/y4s1d4M/bubur-sumsum.png" },
+        Desserts4: { name: "Kue Lapis",      id: "Desserts4", class: "Desserts-Jajanan card-styling", img: "https://i.ibb.co/PZfzXTG/kue-lapis.png" },
+        Desserts5: { name: "Klepon",         id: "Desserts5", class: "Desserts-Jajanan card-styling", img: "https://i.ibb.co/4gXN528/klepon.png" },
+        Desserts6: { name: "Churros",        id: "Desserts6", class: "Desserts-Jajanan card-styling", img: "https://i.ibb.co/whNcpLg/image.png" },
+        Desserts7: { name: "Loukemedes",     id: "Desserts7", class: "Desserts-Jajanan card-styling", img: "https://i.ibb.co/0Yp5XnJ/image.png" },
+        Desserts8: { name: "Ice Cream Boba", id: "Desserts8", class: "Desserts-Jajanan card-styling", img: "https://i.ibb.co/hYvQpr9/image.png" }
     },
     CitizenChoose: {
-        CitizenChoose1: { name: "Pempek Palembang",    id: "CitizenChoose1", class: "CitizenChoose-Popular card-styling", img: "" },
-        CitizenChoose2: { name: "Pempek Kapal Selam",  id: "CitizenChoose2", class: "CitizenChoose-Popular card-styling", img: "" },
+        CitizenChoose1: { name: "Pempek Palembang",    id: "CitizenChoose1", class: "CitizenChoose-Spicy card-styling", img: "https://i.ibb.co/QYL0XBc/Pempek-Palembang.png" },
+        CitizenChoose2: { name: "Pempek Kapal Selam",  id: "CitizenChoose2", class: "CitizenChoose-Spicy card-styling", img: "https://i.ibb.co/r6qRvZP/Pempek-Kapal-Selam.png" },
         CitizenChoose3: { name: "Nasi Goreng Kampung", id: "CitizenChoose3", class: "CitizenChoose-Popular card-styling", img: "https://i.ibb.co/y82pdZw/Nasi-goreng-kampung.png" },
-        CitizenChoose4: { name: "Nasi Goreng Seafood", id: "CitizenChoose4", class: "CitizenChoose-Popular card-styling", img: "" },
-        CitizenChoose5: { name: "Rendang Sapi",        id: "CitizenChoose5", class: "CitizenChoose-Hype card-styling", img: "" },
-        CitizenChoose6: { name: "Rendang Ayam",        id: "CitizenChoose6", class: "CitizenChoose-Hype card-styling", img: "" },
+        CitizenChoose4: { name: "Nasi Goreng Seafood", id: "CitizenChoose4", class: "CitizenChoose-Popular card-styling", img: "https://i.ibb.co/0C1xvBK/Nasi-Goreng-Seafood.png" },
+        CitizenChoose5: { name: "Rendang Sapi",        id: "CitizenChoose5", class: "CitizenChoose-Hype card-styling", img: "https://i.ibb.co/3swRRh8/Rendang-Sapi.png" },
+        CitizenChoose6: { name: "Rendang Ayam",        id: "CitizenChoose6", class: "CitizenChoose-Hype card-styling", img: "https://i.ibb.co/9tx7dM9/Rendang-Ayam.png" },
         CitizenChoose7: { name: "Martabak Telur",      id: "CitizenChoose7", class: "CitizenChoose-Hype card-styling", img: "https://i.ibb.co/P5F7Mh0/Martabak-Telur.png" }
     }
 };
@@ -89,41 +92,51 @@ dropdown.addEventListener('change', () => {
     //add the new elements to the display;
     for (let item in databaseVal) {
         const newDiv = document.createElement('div');
-        const newP = document.createElement('p');
         newDiv.setAttribute('class', databaseVal[item].class);
         newDiv.setAttribute('id', databaseVal[item].id);
         if (wishlist.has(newDiv.id)) {
             newDiv.classList.add('green');
         }
+        const newImg = document.createElement('img');
+        newImg.setAttribute('src', databaseVal[item].img);
+        const newDivChildren = document.createElement('div');
+        newDivChildren.setAttribute('class', 'card-info');
+        const newP = document.createElement('p');
         newP.innerText = databaseVal[item].name;
-        newDiv.append(newP);
+        const newIcon = document.createElement('i');
+        newIcon.setAttribute('class', 'fa-solid fa-circle-check');
+        newIcon.setAttribute('style', 'color: #ffffff;');
+        newDivChildren.append(newP);
+        newDivChildren.append(newIcon);
+        newDiv.append(newImg);
+        newDiv.append(newDivChildren);
         newDiv.addEventListener('click', (e) => {
-            // console.log(e.target, 59);
-            if (wishlist.has(e.target.id)) {
-                wishlist.delete(e.target.id);
+            if (wishlist.has(newDiv.id)) {
+                wishlist.delete(newDiv.id);
                 newDiv.classList.remove('green');
             } else {
-                wishlist.add(e.target.id);
+                wishlist.add(newDiv.id);
                 newDiv.classList.add('green');
             }
+            // console.log(newDiv);
             console.log(wishlist);
         })
-        const newChildren = Array.from(newDiv.children);
-        newChildren.forEach(el => {
-            el.addEventListener('click', (e) => {
-                // console.log(e.target, 59);
-                e.stopPropagation();
-                if (wishlist.has(e.target.closest('div').id)) {
-                    wishlist.delete(e.target.closest('div').id);
-                    newDiv.classList.remove('green');
-                } else {
-                    wishlist.add(e.target.closest('div').id);
-                    newDiv.classList.add('green');
-                }
-                console.log(wishlist);
-            })          
-        })
+        // const newChildren = Array.from(newDiv.children);
+        // newChildren.forEach(el => {
+        //     el.addEventListener('click', (e) => {
+
+        //         e.stopPropagation();
+        //         if (wishlist.has(e.target.closest('div').id)) {
+        //             wishlist.delete(e.target.closest('div').id);
+        //             newDiv.classList.remove('green');
+        //         } else {
+        //             wishlist.add(e.target.closest('div').id);
+        //             newDiv.classList.add('green');
+        //         }
+        //     })          
+        // })
         display.append(newDiv);
+        console.log(wishlist);
     }
     //find how many class in a 1st key database
     const tempSet = new Set();
@@ -143,9 +156,6 @@ dropdown.addEventListener('change', () => {
             childrenArr.forEach(el => {
                 const tempArrClass = el.className.split(' ');
                 const tempArrClass2 = tempArrClass[0].split('-');
-                // tempSet.add(tempArrClass2[1]);        
-                // console.log(el.className, 144);
-                // console.log(filter2, 145);
                 if (tempArrClass2[1] === filter2) {
                     el.classList.remove('display-off');
                 } else {
@@ -171,6 +181,14 @@ randomButton.addEventListener("click", function(event) {
 
     if (tempArr.length < minimumLength) {
         modal.showModal()
+    } else {
+        //remove all element/data in display section
+        const arrFilter = Array.from(filters.children);
+        const arrDisplay = Array.from(display.children);
+        document.querySelector("#dropdown-section").remove();
+        arrFilter.forEach(el => el.remove());
+        arrDisplay.forEach(el => el.remove());
+        randomButton.remove();
     }
 })
 closeModalButton.addEventListener("click", function(event) {
@@ -187,4 +205,4 @@ dialog.addEventListener("click", e => {
     ) {
         dialog.close()
     }
-    })
+})
